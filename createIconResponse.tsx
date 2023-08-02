e91ccdb3d85bd7fddeb8e5946ebadbd76b365215
Nikhil Saraf <nsaraf98@gmail.com>
2023-08-02 16:39:36 +0530

    icons

--- createIconResponse.tsx
+++ createIconResponse.tsx
@@ -54,8 +54,6 @@ export function createIconResponse(
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
         "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
       },
     }
