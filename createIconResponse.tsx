0dd72575c3618f9e975d76d0c4798ce6a6dc5bff
Imran Ansari <imran.ansari@leptonsoftware.com>
2024-08-07 14:28:55 +0530

    Added function and logic to handle marker icon

--- createIconResponse.tsx
+++ createIconResponse.tsx
@@ -122,3 +122,69 @@ export function createImgIconResponse(
     }
   );
 }
+
+export function createMarkerIconResponse(
+  text: string,
+  fillColor: string,
+  strokeColor: string,
+  strokeWidth: number,
+  iconColor: string,
+  width: number,
+  height: number
+) {
+  const widthRem = 4;
+  const heightRem = widthRem * (height / width);
+  const radius = Math.min(width, height) / 2; // Ensure the circle fits within the SVG
+
+  return new ImageResponse(
+    (
+      <Marker
+        fillColor={fillColor}
+        strokeColor={strokeColor}
+        strokeWidth={strokeWidth}
+      >
+        <div
+          tw="absolute w-[128px] h-[128px] flex justify-center items-center -top-4"
+          style={{
+            color: iconColor,
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={widthRem + "rem"}
+            height={heightRem + "rem"}
+            viewBox={`0 0 ${width} ${height}`}
+          >
+            <circle
+              cx={width / 2}
+              cy={height / 2}
+              r={radius}
+              fill={iconColor}
+            />
+            <span
+              tw="relative"
+              style={{
+                color: fillColor,
+                fontSize: "35px",
+                margin: "auto",
+              }}
+            >
+              {text}
+            </span>
+          </svg>
+        </div>
+      </Marker>
+    ),
+    {
+      height: 128,
+      width: 128,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+        "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
+      },
+    }
+  );
+}
